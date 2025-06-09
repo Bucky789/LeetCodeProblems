51a1e14ce17c531ea59b3d5195984c8b5c0c0c9a
Manthan Sumbhe <113107477+Bucky789@users.noreply.github.com>
2025-06-09 20:56:27 +0530

    Update TwoSum.java

--- TwoSum/TwoSum.java
+++ TwoSum/TwoSum.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class TwoSum{
     public static void main(String args[]){
-        int[] nums = {2, 7, 11, 15};  // Example input
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
 
         System.out.println(solution(nums, target));
